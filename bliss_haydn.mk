17183d8ff73d5debbf357e7d3480595fa46213cc
Thunderstorm1708 <89205550+Thunderstorm1708@users.noreply.github.com>
2024-08-16 19:24:24 +0530

    haydn: Bliss-ify!

--- bliss_haydn.mk
+++ bliss_haydn.mk
@@ -11,14 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+
+# Bliss
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
-PRODUCT_NAME := lineage_haydn
+PRODUCT_NAME := bliss_haydn
 
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
@@ -32,5 +37,3 @@ BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:14/UKQ1.231207.002/V816.0.2.0.UKK
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Sign Builds
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/xiaomi/priv-keys/releasekey
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,17 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
 # Common Tree
-git clone --depth=1 https://github.com/Alucard-Storm/android_device_xiaomi_sm8350-common -b lineage-21 device/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/Thunderstorm1708/android_device_xiaomi_sm8350-common -b bliss device/xiaomi/sm8350-common
 
 # Haydn Vendor
-git clone --depth=1 https://github.com/Alucard-Storm/proprietary_vendor_xiaomi_haydn -b lineage-21 vendor/xiaomi/haydn
+git clone --depth=1 https://github.com/Thunderstorm1708/proprietary_vendor_xiaomi_haydn -b lineage-21 vendor/xiaomi/haydn
 
 # Common Vendor
-git clone --depth=1 https://github.com/Alucard-Storm/proprietary_vendor_xiaomi_sm8350-common -b lineage-21 vendor/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/Thunderstorm1708/proprietary_vendor_xiaomi_sm8350-common -b lineage-21 vendor/xiaomi/sm8350-common
 
 # Kernel
 echo 'Cloning kernel tree'
-git clone --depth=1  https://github.com/xiaomi-haydn-devs/android_kernel_xiaomi_sm8350 --recursive -b lineage-21 kernel/xiaomi/sm8350
+git clone --depth=1  https://github.com/Thunderstorm1708/android_kernel_xiaomi_sm8350 --recursive -b lineage-21 kernel/xiaomi/sm8350
 
 # Firmware
 echo 'Cloning firmware'
@@ -26,8 +26,8 @@ echo 'Cloning Leica camera'
 git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera -b fourteen-leica vendor/xiaomi/haydn-miuicamera
 rm -rf hardware/xiaomi/megvii
 
-# Signature Keys
-echo 'Cloning Signing keys'
-git clone --depth=1 https://github.com/xiaomi-haydn-devs/priv_keys.git vendor/xiaomi/priv-keys
+# Priv-keys
+echo 'Cloning private keys'
+git clone https://github.com/1xtAsh/vendor_lineage-priv_keys -b lineage-21 vendor/bliss-priv/keys
 
 echo 'delete vendorsetup.sh from device tree once this is done'
